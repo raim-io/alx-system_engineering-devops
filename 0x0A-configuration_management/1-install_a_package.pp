1b22e25d11868d837336e6c4764968d1c866e12b
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-30 21:11:44 +0100

    fix: fix puppet script for installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install the flask package from pip3
 
-package { 'flask':
-  ensure => '2.1.0',
-  provider    => 'pip3',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
