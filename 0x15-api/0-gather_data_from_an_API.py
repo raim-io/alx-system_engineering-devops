85790e975cf02ffe19723d31a548bc8a45e61a0f
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-07-31 23:54:33 +0100

    feat: gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+"""Return employee TODO list progress, by employee ID"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    hostname = "https://jsonplaceholder.typicode.com"
+    user = requests.get(hostname + "/users/{}".format(sys.argv[1])).json()
+    userTodoList = requests.get(hostname + "/todos", params={"userId": sys.argv[1]}).json()
+
+    completedTasks = [t.get("title") for t in userTodoList if t.get("completed") is True]
+
+    print("Emplyee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(completedTasks), len(userTodoList)))
+    [print("\t {}".format(task)) for task in completedTasks]
